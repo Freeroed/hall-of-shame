d1dbafc0b1c7e5efb022a8150423b7ad66ccdb7b
Сергей Евдокимов <s.evdokimov@bars.group>
2021-04-06 20:33:27 +0300

    Добавлены настройки для запуска на heroku

---
+++ client-server.js
@@ -0,0 +1,10 @@
+var liveServer = require("live-server");
+
+var params = {
+    port: 8080, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "./dist", // Set root directory that's being served. Defaults to cwd.
+    file: "index.html",
+       
+};
+liveServer.start(params);
